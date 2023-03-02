dc7c446169d5d668acf3b94b9d49340ea635ea33
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-03-02 17:34:50 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
 	user: "postgres",
 	password: "admin",
-	host: "db.cqatkeqpwupisfdzxjgq.supabase.co",
+	host: "localhost",
 	port: "5432",
 	database: "postgres"
 })
